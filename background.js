9bba6bc8df212172e06bc2426efdf180a60612fe
AmirN347 <amirn2203@gmail.com>
2026-03-13 23:36:55 -0400

    feat: Extract text from current website and prints to console log

--- background.js
+++ background.js
@@ -2,3 +2,20 @@
 Service worker: This file is for central orchestration and network
 calls. 
 */
+
+chrome.runtime.onInstalled.addListener( () => 
+    {
+    console.log("Extension Installed");
+    }
+);
+
+// Inject and run content script on active tab to scrape all text from DOM
+
+chrome.action.onClicked.addListener( (tab) => 
+{
+    // Inject content script into active tab 
+    chrome.scripting.executeScript({
+        target:{tabId:tab.id},
+        files:["content.js"]
+    });
+})
--- content.js
+++ content.js
@@ -2,3 +2,28 @@
 This file is for page specific logic (reading/modifying DOM). This
 will contain our core logic for extracting text (detecting scams).
 */
+
+console.log("Content Script Loaded");
+
+//Script to extract all text from DOM and log it to console
+//Creating a function to extract test from current DOM
+
+function extractText(){
+    const text = document.body.innerText;
+    console.log(text);
+}
+
+//load the intital text from the DOM
+
+extractText();
+
+//creating a mutation observer to watch for changes in the DOM
+let last_url = location.href
+
+new MutationObserver(() =>{
+        if (location.href !== last_url){
+            last_url = location.href
+            extractText();
+        }
+    }
+).observe(document, {childList:true, subtree:true});
