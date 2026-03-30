8250fc8aaafe749911e3672efdf80d82212845e7
AmirN347 <amirn2203@gmail.com>
2026-03-29 23:56:07 -0400

    feat: Added testing framework

--- website/elder-fraud-protection/vite.config.js
+++ website/elder-fraud-protection/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+  test:{
+    environment:'jsdom',
+    globals:true
+  }
+});
